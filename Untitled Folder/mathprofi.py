d27432e40b27a8f938792d5ea1aa3c9dd037ffe2
solnotka <nataliya.goncharova@gmail.com>
2021-07-09 13:33:26 +0300

    ДЗ-5

---
+++ Untitled Folder/mathprofi.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#print(np.linalg.det([[3, 5, -1,], [5, 3, -7], [-1, -7, -13]]))
+
+#for i in range(17):
+    #print(i)
+    #print(round(6 / (2 - (np.cos(np.pi * i / 8))), 2))
+
+#for i in range(17):
+    #print(i)
+    #try:
+        #print(round(4 / (1 + (np.sin(np.pi * i / 8))), 2))
+    #except ZeroDivisionError:
+        #print('Function is not defined')
+
+for i in range (2, 10):
+    print(np.sqrt(4 * i - 8))
